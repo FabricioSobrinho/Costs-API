5630675a1e23e28762fa9c3a0e17a52b16f67b22
FabricioSobrinho <fabriciomails3@gmail.com>
2024-04-04 15:36:09 -0400

    Gitignore: add gitignore

--- obj/Debug/net8.0/CostsApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/CostsApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CostsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05a4ac14d177e0d569cda0a46838add0eaea887d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CostsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CostsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
