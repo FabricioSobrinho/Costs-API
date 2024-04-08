a1ff8d6b507d25ff99a1c9e616e19aeafda8d041
FabricioSobrinho <fabriciomails3@gmail.com>
2024-04-08 15:00:15 -0400

    Feat: create first project model

---
+++ projects/Project.cs
@@ -0,0 +1,20 @@
+﻿using CostsApi.Entities.Enums;
+
+namespace CostsApi.Projects
+{
+	public class Project
+	{
+		public Guid Id { get; init; }
+		public double Budget { get; set; }
+		public double Cost { get; set; }
+		public Category Category { get; set; }
+
+		public Project(double budget, Category category)
+		{
+			Id = Guid.NewGuid();
+			Budget = budget;
+			Cost = 0;
+			Category = category;
+		}
+	}
+}
---
+++ projects/ProjectsRoutes.cs
@@ -0,0 +1,10 @@
+﻿namespace CostsApi.Projects
+{
+	public static class ProjectsRoutes
+	{
+		public static void AddProjectsRoutes(WebApplication app)
+		{
+			app.MapGet("projects", () => new Project(21000, Entities.Enums.Category.Development));
+		}
+	}
+}
