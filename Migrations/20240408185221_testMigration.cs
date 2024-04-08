42ea6ff7523a3d16fca55dd07a62a73e83acb914
FabricioSobrinho <fabriciomails3@gmail.com>
2024-04-08 15:00:46 -0400

    Feat: add initial sanity migration

---
+++ Migrations/20240408185221_testMigration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CostsApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class testMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Budget = table.Column<double>(type: "double precision", nullable: false),
+                    Cost = table.Column<double>(type: "double precision", nullable: false),
+                    Category = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
