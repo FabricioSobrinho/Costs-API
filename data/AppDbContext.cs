fc2c4236aac05532d13f3c17aca317af06783e97
FabricioSobrinho <fabriciomails3@gmail.com>
2024-04-08 14:59:26 -0400

    Feat: add app db context

---
+++ data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using CostsApi.Projects;
+
+namespace CostsApi.Data
+{
+	public class AppDbContext : DbContext
+	{
+		protected readonly IConfiguration _configuration;
+
+		public AppDbContext(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
+		}
+		public DbSet<Project> Projects { get; set; }
+	}
+}
