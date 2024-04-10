5f472b03a0adfa8dbb91a5b779b5cf5cec9fee2a
FabricioSobrinho <fabriciomails3@gmail.com>
2024-04-10 09:07:38 -0400

    Feat: add services model

---
+++ ProjectServices/ProjectService.cs
@@ -0,0 +1,18 @@
+﻿namespace CostsApi.ProjectServices
+{
+	public class ProjectService
+	{
+		public Guid Id { get; set; }
+        public string ServiceName { get; set; }
+		public double Cost { get; set; }
+		public string Description { get; set; }
+
+		public ProjectService(string serviceName, double cost, string description)
+		{
+			Id = Guid.NewGuid();
+			ServiceName = serviceName;
+			Cost = cost;
+			Description = description;
+		}
+	}
+}
