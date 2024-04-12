07c8213667228ba5a7e8592760368474ae3a5c59
FabricioSobrinho <fabriciomails3@gmail.com>
2024-04-12 18:56:23 -0400

    Feat: add project name field

--- ProjectServices/ProjectService.cs
+++ ProjectServices/ProjectService.cs
@@ -3,13 +3,15 @@
 	public class ProjectService
 	{
 		public Guid Id { get; set; }
+		public string ProjectName { get; set; }
         public string ServiceName { get; set; }
 		public double Cost { get; set; }
 		public string Description { get; set; }
 
-		public ProjectService(string serviceName, double cost, string description)
+		public ProjectService(string projectName, string serviceName, double cost, string description)
 		{
 			Id = Guid.NewGuid();
+			ProjectName = projectName;
 			ServiceName = serviceName;
 			Cost = cost;
 			Description = description;
--- projects/Project.cs
+++ projects/Project.cs
@@ -6,14 +6,16 @@ namespace CostsApi.Projects
 	public class Project
 	{
 		public Guid Id { get; init; }
+		public string ProjectName { get; init; }
 		public double Budget { get; set; }
 		public double Cost { get; set; }
 		public Category Category { get; set; }
 		public List<ProjectService> Services { get; set; }
 
-		public Project(double budget, Category category)
+		public Project(string projectName, double budget, Category category)
 		{
 			Id = Guid.NewGuid();
+			ProjectName = projectName!;
 			Budget = budget;
 			Cost = 0;
 			Category = category;
