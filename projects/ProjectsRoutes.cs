7ec4c5b8079ac5878ff13180a5ce627f22e01fa0
FabricioSobrinho <fabriciomails3@gmail.com>
2024-04-12 18:59:49 -0400

    Feat: add post and get routes

--- projects/ProjectsRoutes.cs
+++ projects/ProjectsRoutes.cs
@@ -1,10 +1,34 @@
-﻿namespace CostsApi.Projects
+﻿using CostsApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CostsApi.Projects
 {
 	public static class ProjectsRoutes
 	{
 		public static void AddProjectsRoutes(WebApplication app)
 		{
-			app.MapGet("projects", () => new Project(21000, Entities.Enums.Category.Development));
+			app.MapGet("projects", async (AppDbContext context) =>
+			{
+				var projects = await context.Projects.ToListAsync();
+
+				return projects;
+			});
+
+			app.MapPost("projects", async (AddProjectRequest request, AppDbContext context) =>
+			{
+				var newProject = new Project(request.ProjectName, request.Budget, request.Category);
+
+				var projectExists = await context.Projects.AnyAsync((project) => project.ProjectName == newProject.ProjectName);
+				
+				if (projectExists)
+				{
+					return Results.Conflict("Projeto já existe");
+				}
+
+				await context.Projects.AddAsync(newProject);
+				await context.SaveChangesAsync();
+				return Results.Ok();
+			});
 		}
 	}
 }
