caf428c7e7f3b20e7b5f845e668447d2a034ccbb
FabricioSobrinho <fabriciomails3@gmail.com>
2024-04-15 14:35:48 -0400

    Feat: add edit fields

--- projects/UpdateProjectRequest.cs
+++ projects/UpdateProjectRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace CostsApi.Projects
+﻿using CostsApi.Entities.Enums;
+
+namespace CostsApi.Projects
 {
-	public record UpdateProjectRequest(string ProjectName);
+	public record UpdateProjectRequest(string ProjectName, double ProjectCosts, double ProjectBudget, Category Category);
 }
