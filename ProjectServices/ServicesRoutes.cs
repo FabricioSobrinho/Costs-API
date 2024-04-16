b8972f7fe1d2f56aaf9bdc6d45e7ab6ed92f7f69
FabricioSobrinho <fabriciomails3@gmail.com>
2024-04-16 12:50:37 -0400

    Feat: create get services route

---
+++ ProjectServices/ServicesRoutes.cs
@@ -0,0 +1,18 @@
+﻿using CostsApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CostsApi.ProjectServices
+{
+	public class ServicesRoutes
+	{
+		public static void AddServicesRoutes(WebApplication app)
+		{
+			app.MapGet("services/:{ProjectName}", async (string ProjectName, AppDbContext context, CancellationToken ct) =>
+			{
+				var services = await context.Services.Where(service => service.ProjectName == ProjectName).ToListAsync();
+
+				return Results.Ok(services);
+			});
+		}
+	}
+}
