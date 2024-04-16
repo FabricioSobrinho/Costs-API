e5933a12a0560e5e745530741cc3780f9e0d238a
FabricioSobrinho <fabriciomails3@gmail.com>
2024-04-16 13:25:14 -0400

    Feat: update migrations

---
+++ Migrations/20240416172103_addServiceConstraints.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CostsApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class addServiceConstraints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Services_ServiceName",
+                table: "Services",
+                column: "ServiceName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Services_ServiceName",
+                table: "Services");
+        }
+    }
+}
