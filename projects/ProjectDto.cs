467ea39ccc2e0e2c13560a8d115109432e082420
FabricioSobrinho <fabriciomails3@gmail.com>
2024-04-16 14:24:28 -0400

    Feat: add services list at dto

--- projects/ProjectDto.cs
+++ projects/ProjectDto.cs
@@ -1,6 +1,7 @@
 ﻿using CostsApi.Entities.Enums;
+using CostsApi.ProjectServices;
 
 namespace CostsApi.Projects
 {
-	public record ProjectDto(string ProjectName, double Cost, double Budget, Category Category);
+	public record ProjectDto(string ProjectName, double Cost, double Budget, Category Category, List<ProjectServiceDto> Services);
 }
