e85f4f15cb76799b67c341e95d3407ff97a5c102
FabricioSobrinho <fabriciomails3@gmail.com>
2024-04-16 13:15:56 -0400

    Feat: add post services route

--- ProjectServices/ServicesRoutes.cs
+++ ProjectServices/ServicesRoutes.cs
@@ -1,4 +1,5 @@
 ﻿using CostsApi.Data;
+using CostsApi.ProjectServices.Request_Records;
 using Microsoft.EntityFrameworkCore;
 
 namespace CostsApi.ProjectServices
@@ -9,9 +10,28 @@ namespace CostsApi.ProjectServices
 		{
 			app.MapGet("services/:{ProjectName}", async (string ProjectName, AppDbContext context, CancellationToken ct) =>
 			{
-				var services = await context.Services.Where(service => service.ProjectName == ProjectName).ToListAsync();
+				var services = await context.Services.Where(service => service.ProjectName == ProjectName).ToListAsync(ct);
 
 				return Results.Ok(services);
+			});
+
+			app.MapPost("services", async (AddServiceRequest request, AppDbContext context, CancellationToken ct) =>
+			{
+				var newService = new ProjectService(request.ProjectName, request.ServiceName, request.Cost, request.Description);
+
+				var project = await context.Projects.FirstOrDefaultAsync(project => project.ProjectName ==  request.ProjectName, ct);
+				
+				if (project == null)
+				{
+					return Results.NotFound("Projeto não existe");
+				}
+				
+				project.Services.Add(newService);
+
+				await context.Services.AddAsync(newService, ct);
+				await context.SaveChangesAsync(ct);
+
+				return Results.Ok(newService);
 			});
 		}
 	}
