9f0e5ebecf6d4ea1a44a5370cab086c0694a5ea4
FabricioSobrinho <fabriciomails3@gmail.com>
2024-04-17 14:49:45 -0400

    Feat: create get DTO

---
+++ projects/ProjectGetDto.cs
@@ -0,0 +1,7 @@
+﻿using CostsApi.Entities.Enums;
+using CostsApi.ProjectServices;
+
+namespace CostsApi.Projects
+{
+	public record ProjectGetDto(Guid Id, string ProjectName, double Cost, double Budget, Category Category, List<ProjectServiceDto> Services);
+}
